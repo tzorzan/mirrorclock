1b5b40d6ac041a4e00dda34fc9b755fed531fe0c
Tobia Zorzan <tobia.zorzan@gmail.com>
2016-01-21 16:11:43 +0100

    Added day label and number.

--- js/main.js
+++ js/main.js
@@ -3,16 +3,22 @@ $(document).ready(function() {
     setInterval('updateClock()', 500 );
 });
 
+var dayLabels = ['LUN', 'MAR', 'MER', 'GIO', 'VEN', 'SAB', 'DOM'];
+
 function updateClock() {
     //Current Time Variables
     var currentTime = new Date ( );
+    var currentWeekDay = currentTime.getDay();
+    var currentDay = currentTime.getDate();
     var currentHours = currentTime.getHours();
     var currentMinutes = currentTime.getMinutes();
 
     //Zerolead the minutes for display
     currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
 
+    jQuery("#clock-weekday").html(dayLabels[currentWeekDay]);
+    jQuery("#clock-day").html(currentDay);
+    jQuery("#clock-hours").html(currentHours);
     jQuery("#clock-hours").html(currentHours);
     jQuery("#clock-minutes").html(currentMinutes);
-
 };
